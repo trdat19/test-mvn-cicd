32c70bafb18991d497e1f362f3649cd06ac55539
TruongThanhDat <truongthanhdat019@gmail.com>
2026-05-01 11:59:51 +0700

    resolve checkstyle

--- src/main/java/example/mvntest/HelloApplication.java
+++ src/main/java/example/mvntest/HelloApplication.java
@@ -6,11 +6,13 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        URL source = HelloApplication.class.getResource("hello-view.fxml");
+        FXMLLoader fxmlLoader = new FXMLLoader(source);
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
